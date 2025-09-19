b3d2cd58b711fcf32de772e11b6d295327516935
tejaswinisawant15 <tejaswini1576@gmail.com>
2025-09-19 11:55:33 +0530

    Add files via upload

---
+++ largest_two.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a > b)
+        printf("%d is larger.\n", a);
+    else if (b > a)
+        printf("%d is larger.\n", b);
+    else
+        printf("Both numbers are equal.\n");
+
+    return 0;
+}
